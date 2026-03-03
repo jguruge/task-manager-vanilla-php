8272020a233bfc824fa2766da149240a9425d079
jayashan <jguruge2021@gmail.com>
2026-03-03 16:31:19 +0530

    implement login and basic login ui  to check

---
+++ public/login.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+require_once "../models/User.php";
+
+
+$message = "";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    $user = new User();
+
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    if ($user->login($email, $password)) {
+        header("Location: dashboard.php");
+        exit;
+    } else {
+        $message = "Invalid credentials!";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+
+    <form method="POST">
+        <input type="email" name="email" placeholder="Email" required><br><br>
+        <input type="password" name="password" placeholder="Password" required><br><br>
+        <button type="submit">Login</button>
+    </form>
+
+    <p><?= $message ?></p>
+
+    <a href="register.php">Register Here</a>
+</body>
+</html>
