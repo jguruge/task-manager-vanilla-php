d9a4da32f7340dd8f69ffb43ffb448e6f5373a12
jayashan <jguruge2021@gmail.com>
2026-03-03 16:52:37 +0530

    Add CSS and JavaScript enhancements to login page

---
+++ public/assets/js/login.js
@@ -0,0 +1,9 @@
+function togglePassword() {
+    const passwordInput = document.getElementById("password");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+    } else {
+        passwordInput.type = "password";
+    }
+}
